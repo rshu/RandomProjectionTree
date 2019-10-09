37f13adb7fc675ed3edc4c2768bab6b76b80f4dd
Rui Shu <terry.shu@hotmail.com>
2019-10-08 23:53:18 -0400

    add 3d plot

--- Misc/test.py
+++ Misc/test.py
@@ -1,21 +1,26 @@
 import random
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from collections import OrderedDict
+from operator import getitem
+import math
 
-N_POINT = 10000
-N_DIMENSION = 2
-
-# an example of two dimension space
-random.seed(30)
-
-
-def random_point(dimension=2, min_c=0, max_c=N_POINT):
-    return [random.randint(min_c, max_c) for n,_ in enumerate(range(dimension))]
+# parameter initialization
+N_POPULATION = 500
+N_DIMENSION = 3
+N_POLAR = int(math.log(math.sqrt(N_POPULATION)) + 1)
+MAX_RANGE = 10000
+MIN_RANGE = 0
+random.seed(12345)
 
 
-def distance(p1=[], p2=[]):
-    d = 0
-    d += (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
-    return d
+def random_point(dimension=N_DIMENSION, min_c=MIN_RANGE, max_c=MAX_RANGE):
+    return [random.randint(min_c, max_c) for _, _ in enumerate(range(dimension))]
+
+
+def sqd(p1, p2):
+    return int(math.sqrt(sum((c1 - c2) ** 2 for c1, c2 in zip(p1, p2))))
 
 
 def projection(p=[], polar=[]):
@@ -23,28 +28,90 @@ def projection(p=[], polar=[]):
     right = [polar[2], polar[3]]
 
     # find point near left or right
-    if distance(p, left) <= distance(p, right):
+    if sqd(p, left) <= sqd(p, right):
         return 0  # near the left point
     else:
         return 1  # near the right point
 
 
+def RPTree():
+    pass
+
+
 def main():
-    pop = [random_point() for _ in range(1000)]
-    # print(listOfRandomPoint)
+    # initialize population with random points
+    pop = [random_point() for _ in range(N_POPULATION)]
+    # print(pop)
+    xs = [p[0] for p in pop]
+    ys = [p[1] for p in pop]
+    zs = [p[2] for p in pop]
+
+    # plot pop in 3D space
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xs, ys, zs, marker='o')
+    plt.title('Random population in 3D space')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    # plt.show()
+
+    # generate N_POLAR**N_POLAR polars
+    # however, we want to avoid short polars
+    polar = {}
+    print(N_POLAR ** N_POLAR)
+
+    for i in range(N_POLAR ** N_POLAR):  # how to sample
+        randomP1 = random.choice(pop)
+        randomP2 = random.choice(pop)
+        polar[i] = {}
+        polar[i]['P1'] = randomP1
+        polar[i]['P2'] = randomP2
+        polar[i]['Distance'] = sqd(randomP1, randomP2)
+
+    # print("polar", polar)
+    sortedPolar = sorted(polar.items(), key=lambda x: x[1]['Distance'])
+    sortedPolar.reverse()
+    # print("sortedpolar", sortedPolar)
+
+    # only keep top N_POLAR polars
+    finalPolarList = [sortedPolar[i] for i in range(N_POLAR)]
+    finalPolar = dict(finalPolarList)  # convert list to dict
+    print(finalPolar)
+
+    for key1, value1 in finalPolar.items():
+        print(value1)
+        p1 = value1.get("P1")
+        p2 = value1.get("P2")
+        plt.plot(p1, p2, marker='.', linewidth=2)
+
+    # find the longest line
+    pop.sort(key=lambda x: x[0])
+    midP = pop[int(N_POPULATION / 2)]
+
+    ds = [sqd(i, midP) for i in pop]
+    east = pop[ds.index(max(ds))]
+    ds = [sqd(i, east) for i in pop]
+    west = pop[ds.index(max(ds))]
+
+    print("east: ", east, "west:", west, "Distance: ", sqd(east, west))
+    plt.plot(east, west, 'k^-')
+    plt.show()
+
+    exit(1)
+
     polar = []
 
     x = [c[0] for c in pop]
-    y = [c[1] for c in pop]
     y = [c[1] for c in pop]
     pop = sorted(pop, key=lambda l: l[1], reverse=True)
     randomP = pop[500]
     # print(randomP)
 
     # find the longest polar
-    ds = [distance(i, randomP) for i in pop]
+    ds = [sqd(i, randomP) for i in pop]
     east = pop[ds.index(max(ds))]
-    ds = [distance(i, east) for i in pop]
+    ds = [sqd(i, east) for i in pop]
     west = pop[ds.index(max(ds))]
 
     if east[0] < west[0]:
